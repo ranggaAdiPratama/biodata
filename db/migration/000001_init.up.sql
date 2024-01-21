5c0bdb7e86e868c745d9b94b6c52aa3982f8c322
Rangga Adi Pratama <ranggaadipratama96@yahoo.com>
2024-01-21 20:56:55 +0700

    feat: seeding

--- db/migration/000001_init.up.sql
+++ db/migration/000001_init.up.sql
@@ -1,26 +1,20 @@
-CREATE TABLE
-    "users" (
-        "id" bigserial PRIMARY KEY,
-        "username" varchar UNIQUE NOT NULL,
-        "name" varchar NOT NULL,
-        "email" varchar UNIQUE NOT NULL,
-        "password" varchar NOT NULL,
-        "profile_picture" varchar,
-        "created_at" timestamp NOT NULL DEFAULT (now()),
-        "updated_at" timestamp
-    );
+CREATE TABLE "users" (
+    "id" bigserial PRIMARY KEY, "username" varchar UNIQUE NOT NULL, "name" varchar NOT NULL, "email" varchar UNIQUE NOT NULL, "password" varchar NOT NULL, "profile_picture" varchar, "created_at" timestamp NOT NULL DEFAULT(now()), "updated_at" timestamp
+);
 
-CREATE TABLE
-    "hobbies" (
-        "id" bigserial PRIMARY KEY,
-        "user_id" bigserial NOT NULL,
-        "name" varchar NOT NULL,
-        "created_at" timestamp NOT NULL DEFAULT (now()),
-        "updated_at" timestamp
-    );
+CREATE TABLE "hobbies" (
+    "id" bigserial PRIMARY KEY, "user_id" bigserial NOT NULL, "name" varchar NOT NULL, "created_at" timestamp NOT NULL DEFAULT(now()), "updated_at" timestamp
+);
 
 CREATE INDEX ON "hobbies" ("user_id");
 
 ALTER TABLE "hobbies"
-ADD
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+INSERT INTO
+    "users" (
+        "id", "username", "name", "email", "password", "profile_picture", "created_at", "updated_at"
+    )
+VALUES (
+        1, 'rangga', 'Rangga Adi Pratama', 'masterrangga@gmail.com', '$2a$10$nYJnqTID3BLwcEZICroXnOj37Lt1gbEVppPlvnNdSsc2CFbNpkbJ2', NULL, '2024-01-21 13:46:27.463394', NULL
+    );
